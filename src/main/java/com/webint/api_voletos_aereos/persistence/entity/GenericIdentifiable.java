85e3f266d17a64793b1dc624119021830a85e886
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-31 15:33:25 -0500

    lo oculto mediante @JsonIgnore

--- src/main/java/com/webint/api_voletos_aereos/persistence/entity/GenericIdentifiable.java
+++ src/main/java/com/webint/api_voletos_aereos/persistence/entity/GenericIdentifiable.java
@@ -1,5 +1,9 @@
 package com.webint.api_voletos_aereos.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface GenericIdentifiable<ID> {
+
+    @JsonIgnore
     ID getID();
 }
---
+++ src/main/java/com/webint/api_voletos_aereos/web/controller/AerolineController.java
@@ -0,0 +1,2 @@
+package com.webint.api_voletos_aereos.web.controller;public class AerolineController {
+}
